99e7b7f568a1ce399a166b7813910031e3512327
catinahatisback <catinahatisback@gmail.com>
2022-12-16 00:27:35 +0700

    clean

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio/',
+  base: '/Sudoku_solver_react/',
   plugins: [react()],
 })
